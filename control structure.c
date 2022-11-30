067a5596278f68189e3a48db35900ac596a9f12a
sanjayparmar143 <118906048+sanjayparmar143@users.noreply.github.com>
2022-11-30 17:24:20 +0530

    Add files via upload

---
+++ control structure.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+main(){
+	
+	int AGE;
+	
+	printf("Enter AGE : ");
+	scanf("%d",& AGE);
+	
+	if(AGE>18){
+		printf("driving licence eligible\n");
+    }
+    
+    else{
+	
+	 printf("driving licence not eligible");
+	}
+}
